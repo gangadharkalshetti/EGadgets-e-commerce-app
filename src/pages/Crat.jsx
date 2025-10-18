7e0a063f513b38e6e14763ad43be216526e41dd1
gnagadhar kalshetti <gangadharkalshetti16@gmail.com>
2025-10-18 23:00:20 +0530

    add initial page components for About, Collection, Contact, Crat, Home, Login, Orders, PlaceOrder, and Ptoduct

---
+++ src/pages/Crat.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Crat() {
+  return (
+    <div>Crat</div>
+  )
+}
+
+export default Crat
---
+++ src/pages/Ptoduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Ptoduct() {
+  return (
+    <div>Ptoduct</div>
+  )
+}
+
+export default Ptoduct
